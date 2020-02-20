dfd9799d99dea4a1038d35282eea1e59e52b8c50
udaykumar572 <60086619+udaykumar572@users.noreply.github.com>
2020-02-20 18:14:49 +0530

    Add files via upload

---
+++ SpringMvc/src/main/java/SpringMvc/SpringMvc/App.java
@@ -0,0 +1,13 @@
+package SpringMvc.SpringMvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
